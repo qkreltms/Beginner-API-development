90bd1925684f391a323756f9d26fdcd2f0b8a5ed
qkreltms <wfr112@naver.com>
2018-06-01 20:03:40 +0900

    180601803

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -5,7 +5,7 @@ import Restaurant from '../model/restaurant'
 export default({ config, db }) => {
   let api = Router()
 
-  // '/v1/restaurant/add'
+  // '/v1/restaurant/add' -create data
   api.post('/add', (req, res) => {
     let newRest = new Restaurant()
     newRest.name = req.body.name
@@ -17,5 +17,18 @@ export default({ config, db }) => {
       res.json({ message: req.body.name+' Restaurant saved successfully' })
     })
   })
+
+// '/v1/restaurant' - read
+  api.get('/', (req, res) => {
+    Restaurant.find({}, (err, restaurants) => {
+      if (err) {
+        res.send(err)
+      }
+      res.json(restaurants)
+    })
+  })
+
+
+
   return api
 }
